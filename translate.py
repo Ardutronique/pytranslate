8449525c27f4e944f1a345f60acf1150c3e113b4
yannis-mlgrn <yannismlgorn@gmail.com>
2020-04-27 22:37:04 +0200

    add a whyle and text

--- translate.py
+++ translate.py
@@ -1,15 +1,25 @@
 from googletrans import *
+import time
 
 translator = Translator()
-phrase = input("entrer une phrase: ")
 
 def translate():
 	#result = translator.translate(phrase, src='fr', dest='en')
+	phrase = input("entrer une phrase: ")
 	translations = translator.translate(phrase, src='fr', dest='en')
+	print("...")
+	time.sleep(0.5)
 	print(' ',translations.origin, '----->', translations.text)
+	print("...\n")
 
 
-translate()
+restart = True 
+
+while restart:
+	translate()
+	time.sleep(1)
+
+
 
 
 
